d7ff728cd015151d771335bdbf9c27c29899bf4a
telecomshy <telecomshy@189.cn>
2024-03-25 00:01:53 +0800

    add banner_setter docstring and annotation

--- README.rst
+++ README.rst
@@ -1,19 +1,20 @@
 Cool Login Banner
 ==================
 
-说明
+简单说明
 ---------
 这个小玩意，可以快捷方便的定制你的linux登录界面，比如:
 
 .. code-block::
+
     from cool_login_banner import CoolLoginBanner, CowsayEngine
 
     clb = CoolLoginBanner(CowsayEngine, host='192.168.0.33', user='username', password='passoword')
     clb.set_ssh_banner(text="A python enthusiast's site", animal_name='cow', fore_color='lightyellow_ex', styles=['blink'])
 
 
-然后再次登录，你的登录界面就变成这样了：
+然后再次登录，你的登录界面就会变成这样：
 
-.. image:: docs/img/login_banner.gif
+.. image:: ./docs/img/login_banner.gif
     :width: 300
 
--- cool_login_banner/banner_setter.py
+++ cool_login_banner/banner_setter.py
@@ -2,16 +2,23 @@ from fabric import Connection
 from invoke import sudo
 from functools import partial
 from io import BytesIO
+from typing import ClassVar, Any
+from .engines import Engine
 
 
 class BannerSetter:
-    SSHD_CONFIG_PATH = '/etc/ssh/sshd_config'
-    SSH_BANNER_PATH = '/etc/ssh/login_banner'
-    MOTD_PATH = '/etc/motd'
-    ISSUE_PATH = '/etc/issue'
-    ISSUE_NET_PATH = '/etc/issue_net'
+    SSHD_CONFIG_PATH: ClassVar[str] = '/etc/ssh/sshd_config'
+    SSH_BANNER_PATH: ClassVar[str] = '/etc/ssh/login_banner'
+    MOTD_PATH: ClassVar[str] = '/etc/motd'
+    ISSUE_PATH: ClassVar[str] = '/etc/issue'
+    ISSUE_NET_PATH: ClassVar[str] = '/etc/issue_net'
 
-    def __init__(self, engine, *, host=None, port=22, user=None, password=None, encoding='utf8', **kwargs):
+    def __init__(self, engine: type(Engine), *, host: str | None = None, port: int = 22, user: str | None = None,
+                 password: str | None = None, encoding: str = 'utf8', **kwargs):
+        """用户需要有sudo权限，如果没有设置免密，还需要设置password参数。所有关键字参数在内部都会传递给
+        ``fabric.Connection`` 构造函数
+
+        """
         self.engine = engine()
         if host:
             connect_kwargs = kwargs.setdefault('connect_kwargs', {})
@@ -21,58 +28,103 @@ class BannerSetter:
         else:
             self.sudo = partial(sudo, encoding=encoding, password=password, hide=True)
 
-    def _save_text_to_file(self, text, file):
+    def _save_text_to_file(self, text: str, file: str) -> None:
+        """将字符串保存到文件中
+
+        由于banner中包含各种字符，使用bash命令将内容保存到文件总是报非法字符错误，因此用了一个取巧的办法。
+        直接保存为 BytesIO 文件，上传到服务器，然后再修改文件归属。
+
+        """
         with BytesIO(text.encode('utf8')) as f:
             self.conn.put(f, '/tmp/tmp_banner')
             self.sudo(f'mv -f /tmp/tmp_banner {file}')
             self.sudo(f'chown root:root {file}')
 
-    def _clear_file_content(self, file):
+    def _clear_file_content(self, file: str) -> None:
+        """清空文件内容"""
+
         self.sudo(f'sed -i "/^/d" {file}')
 
-    def save_ssh_banner(self, banner):
+    def save_ssh_banner(self, banner: str) -> None:
+        """将banner保存到指定文件，配置/etc/ssh/sshd_config文件，并重启sshd服务"""
+
         self.sudo(f"sed -i -r '/^#?Banner/a Banner {self.SSH_BANNER_PATH}' {self.SSHD_CONFIG_PATH}")
         self._save_text_to_file(banner, self.SSH_BANNER_PATH)
         self.sudo('systemctl restart sshd')
 
-    def set_ssh_banner(self, *args, **kwargs):
+    def set_ssh_banner(self, *args: Any, **kwargs: Any) -> None:
+        """生成ssh登录banner并进行相应配置，参数会直接送给引擎的 generate_banner 方法"""
+
         banner = self.generate_banner(*args, **kwargs)
         self.save_ssh_banner(banner)
 
-    def clear_ssh_banner(self):
+    def clear_ssh_banner(self) -> None:
+        """删除ssh登录banner文件，将/etc/ssh/sshd_config恢复默认值，并重启sshd服务"""
+
         self.sudo(f'rm -f {self.SSH_BANNER_PATH}')
         self.sudo(f'sed -i /^Banner/d {self.SSHD_CONFIG_PATH}')
         self.sudo('systemctl restart sshd')
 
-    def save_motd_banner(self, banner):
+    def save_motd_banner(self, banner: str) -> None:
+        """将banner保存到/etc/motd文件"""
+
         self._save_text_to_file(banner, self.MOTD_PATH)
 
-    def clear_motd_banner(self):
+    def clear_motd_banner(self) -> None:
+        """清空/etc/motd文件"""
+
         self._clear_file_content(self.MOTD_PATH)
 
-    def set_motd_banner(self, *args, **kwargs):
+    def set_motd_banner(self, *args: Any, **kwargs: Any) -> None:
+        """生成banner并保存到/etc/motd文件，参数会送到引擎的generate_banner方法
+
+        motd文件的内容，会在登录成功后显示
+
+        """
         banner = self.generate_banner(*args, **kwargs)
         self.save_motd_banner(banner)
 
-    def save_tty_banner(self, banner):
+    def save_tty_banner(self, banner: str) -> None:
+        """将banner保存到/etc/issue文件"""
+
         self._save_text_to_file(banner, self.ISSUE_PATH)
 
-    def clear_tty_banner(self):
+    def clear_tty_banner(self) -> None:
+        """清空/etc/issue文件"""
+
         self._clear_file_content(self.ISSUE_PATH)
 
-    def set_tty_banner(self, *args, **kwargs):
+    def set_tty_banner(self, *args: Any, **kwargs: Any) -> None:
+        """生成banner并保存到/etc/issue文件，参数会送到引擎的generate_banner方法
+
+        issue文件的内容，会在本机使用tty登录之前显示，ssh远程登录应使用set_ssh_banner方法
+
+        """
+
         banner = self.generate_banner(*args, **kwargs)
         self.save_tty_banner(banner)
 
-    def save_telnet_banner(self, banner):
+    def save_telnet_banner(self, banner: str) -> None:
+        """将banner保存到/etc/issue_net文件"""
+
         self._save_text_to_file(banner, self.ISSUE_NET_PATH)
 
-    def clear_telnet_banner(self):
+    def clear_telnet_banner(self) -> None:
+        """清空/etc/issue_net文件"""
+
         self._clear_file_content(self.ISSUE_NET_PATH)
 
-    def set_telnet_banner(self, *args, **kwargs):
+    def set_telnet_banner(self, *args: Any, **kwargs: Any) -> None:
+        """生成banner并保存到/etc/issue_net文件，参数会送到引擎的generate_banner方法
+
+        issue_net文件的内容，会在使用telnet登录之前显示，ssh远程登录应使用set_ssh_banner方法
+
+        """
+
         banner = self.generate_banner(*args, **kwargs)
         self.save_telnet_banner(banner)
 
-    def __getattr__(self, name):
+    def __getattr__(self, name: str) -> Any:
+        """其它方法和属性转接到engine实例"""
+
         return getattr(self.engine, name)
