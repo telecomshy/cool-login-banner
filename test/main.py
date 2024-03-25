1b4599867e2f7aff9ef6f45e7779064a68e44757
admin <18907190693@189.cn>
2024-03-25 09:32:55 +0800

    fix test

--- test/main.py
+++ test/main.py
@@ -3,7 +3,7 @@ from pyfiglet import figlet_format, Figlet
 from colorama import init, Back, Fore, just_fix_windows_console
 from termcolor import colored
 
-clb = BannerSetter(CowsayEngine, host='192.168.44.131', user='shy', password='shy501024')
+clb = BannerSetter(CowsayEngine, host='192.168.44.131', user='root', password='shy501024')
 # cl.preview_fore_colors()
 
 # clb.set_ssh_banner(text="A python enthusiast's site", animal_name='cow', fore_color='lightyellow_ex', styles=['blink'])
