5c73552716e206d64175cdc5949080c4bbdb49aa
telecomshy <telecomshy@189.cn>
2024-04-01 22:07:46 +0800

    modify some docstrings

--- tests/test_banner_setter.py
+++ tests/test_banner_setter.py
@@ -1,23 +1,23 @@
-from cool_login_banner import BannerSetter, TextEngine
-from unittest.mock import patch
+from cool_login_banner import FigletEngine, BannerSetter
+from unittest.mock import call, sentinel, patch
 
 
 def test_init_banner_setter():
-    banner = BannerSetter()
-    assert banner.engine is None
-    banner = BannerSetter(TextEngine)
-    isinstance(banner.engine, TextEngine)
-
-    # 测试BannerSetter是否可以正常接收Engine参数
-    with patch('cool_login_banner.banner_setter.Connection') as mock_connection:
-        with patch('cool_login_banner.banner_setter.partial') as mock_partial:
-            banner = BannerSetter(host='fakehost')
-            mock_connection.assert_called_once()
-            mock_partial.assert_called_once()
+    setter = BannerSetter()
+    assert setter.engine is None
+    setter = BannerSetter(FigletEngine)
+    assert isinstance(setter.engine, FigletEngine)
 
 
-def test_save_text_to_file(banner_setter):
-    banner_setter._save_text_to_file('content', 'file')
-    banner_setter.conn.put.assert_called_once()
-    banner_setter.sudo.assert_any_call('mv -f /tmp/tmp_banner file')
-    banner_setter.sudo.assert_any_call('chown root:root file')
+def test_init_banner_setter_host_arg(capsys):
+    setter = BannerSetter()
+    assert setter.conn is None
+    assert setter.sudo is sentinel.partial_sudo
+
+
+def test_save_text_to_file():
+    setter = BannerSetter(host="host")
+    setter._save_text_to_file("faketext", "fakefile")
+    calls = [call("mv -f /tmp/tmp_banner fakefile"), call("chown root:root fakefile")]
+    assert setter.sudo.call_args_list == calls
+
