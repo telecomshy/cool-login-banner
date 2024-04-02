faca8fedce2566a403faacd43f6f88ca1bfe0295
admin <18907190693@189.cn>
2024-04-02 08:54:23 +0800

    add save_test_to_file test

--- tests/test_banner_setter.py
+++ tests/test_banner_setter.py
@@ -1,5 +1,5 @@
 from cool_login_banner import FigletEngine, BannerSetter
-from unittest.mock import call, sentinel, patch, ANY
+from unittest.mock import call, patch, ANY
 
 
 def test_init_banner_setter():
@@ -14,8 +14,11 @@ def test_init_banner_setter_with_host():
     assert setter.conn is None
 
     setter = BannerSetter(host="fake_host")
-    setter.conn.host = "fake_host"
+    assert setter.conn.host == "fake_host"
 
 
-
-
+def test_save_text_to_file(banner_setter):
+    banner_setter._save_text_to_file("content", "fakefile")
+    banner_setter.conn.put.assert_called_once_with(ANY, "/tmp/tmp_banner")
+    calls = [call("mv -f /tmp/tmp_banner fakefile"), call("chown root:root fakefile")]
+    assert banner_setter.sudo.call_args_list == calls
